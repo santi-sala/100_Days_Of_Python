d83d878fc688a486c08cfbde4d2f9002e4a7cc3e
Santiago Sala <pipo0716@gmail.com>
2024-11-05 13:44:25 +0200

    1st day Completed

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,7 @@
-# Write your code below this line 👇
-print("Hello world!")
+print("Hello whoever you are!! lets start!")
+user_name = input("What is your name? ")
+print(f"Hello {user_name}!")
+user_city = input("Where do you live? ")
+pet_name = input("What is the name of your pet? ")
+print(f"Your band name could be {user_city} {pet_name}")
+
