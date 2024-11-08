b513a6bc740034ab7249042d8cdb850de69ba536
Santiago Sala <pipo0716@gmail.com>
2024-11-08 23:08:10 +0200

    Day 6 completed

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -2,3 +2,6 @@ def my_function():
     print("Hello World")
 
 my_function()
+
+
+# Completed Reeborgs Hurdle 1-4 and Maze https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
