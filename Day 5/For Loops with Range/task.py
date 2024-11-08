559cd987e1fac011a1177078d206fcca523f399a
Santiago Sala <pipo0716@gmail.com>
2024-11-08 16:33:41 +0200

    Day 6 almost completed

--- Day 5/For Loops with Range/task.py
+++ Day 5/For Loops with Range/task.py
@@ -13,3 +13,5 @@ for number in range(1, 101):
     print(f"Current number is: {current_number} and total is: {total_number}")
 
 print(f"The sum of all numbers from 1 to 100 is: {total_number}")
+
+#Completed Hurdle 1 and Hurdle 2 from: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
