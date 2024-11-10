dff56e7b6e7934abe1a101e272ad094dc664cf65
Santiago Sala <pipo0716@gmail.com>
2024-11-10 10:29:31 +0200

    Day 9 almost completed.

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,12 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "Kpasa"
+
+for thing in programming_dictionary:
+    print(thing)
+    print(programming_dictionary[thing])
